b11e433ff52e77d11d8ae753fed9da45fc7d312c
Thomasnmorrell <36220486+Thomasnmorrell@users.noreply.github.com>
2018-12-02 20:22:29 -0500

    Update subscribe.js

--- tutorials/1-start/pages/subscribe.js
+++ tutorials/1-start/pages/subscribe.js
@@ -1,30 +1,39 @@
 import React from 'react';
-// other imports
+import Head from 'next/head';
 import TextField from 'material-ui/TextField';
 import Button from 'material-ui/Button';
-
+import { styleTextField } from '../components/SharedStyles';
+import withLayout from '../lib/withLayout';
 class Subscribe extends React.Component {
   onSubmit = (e) => {
-    // check if email is missing, return undefined
-    // if email exists, call subscribeToNewsletter() API method
+    // some code
   };
 render() {
     return (
-      // form with input and button
-      <form onSubmit={this.onSubmit}>
-  <p>We will email you when a new tutorial is released:</p>
-  <TextField
-    type="email"
-    label="Your email"
-    style={styleTextField}
-    required
-  />
-  <p />
-  <Button variant="raised" color="primary" type="submit">
-    Subscribe
-  </Button>
-</form>
+      <div style={{ padding: '10px 45px' }}>
+        <Head>
+          <title>Subscribe</title>
+          <meta name="description" content="description for indexing bots" />
+        </Head>
+        <br />
+        <form onSubmit={this.onSubmit}>
+          <p>We will email you when a new tutorial is released:</p>
+          <TextField
+            inputRef={(elm) => {
+              this.emailInput = elm;
+            }}
+            type="email"
+            label="Your email"
+            style={styleTextField}
+            required
+          />
+          <p />
+          <Button variant="raised" color="primary" type="submit">
+            Subscribe
+          </Button>
+        </form>
+      </div>
     );
   }
 }
-export default Subscribe;
+export default withLayout(Subscribe);
