cdba5f5d64aba8d3b11dc6acf931db8b6d4f0917
Thomasnmorrell <36220486+Thomasnmorrell@users.noreply.github.com>
2018-12-02 20:07:56 -0500

    Create subscribe.js

---
+++ tutorials/1-start/pages/subscribe.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// other imports
+class Subscribe extends React.Component {
+  onSubmit = (e) => {
+    // check if email is missing, return undefined
+    // if email exists, call subscribeToNewsletter() API method
+  };
+render() {
+    return (
+      // form with input and button
+    );
+  }
+}
+export default Subscribe;
